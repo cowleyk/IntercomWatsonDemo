ab9ce294b425efb55c0665b59e03fe611d78bbc9
cowleyk <cowley.kevin.l@gmail.com>
2017-07-19 14:59:15 -0600

    Write test for service

--- tests/test_intercom_watson.py
+++ tests/test_intercom_watson.py
@@ -6,7 +6,13 @@ from unittest.mock import ANY, MagicMock, patch
 
 class TestIntercomWatson(NioServiceTestCase):
 
-    service_name = 'IntercomWatson'
+    service_name = 'IntercomWatsonDemo'
+
+    def setUp(self):
+        with patch('blocks.slack.slack_block.Slacker') as patched_slack:
+            self.mocked_slack = MagicMock()
+            patched_slack.return_value.chat.post_message = self.mocked_slack
+            super().setUp()
 
     def env_vars(self):
         return {
@@ -17,17 +23,39 @@ class TestIntercomWatson(NioServiceTestCase):
             'SLACK_API_TOKEN': 'apiToken',
         }
 
-    def mock_modules(self):
-        pass
+    def mock_blocks(self):
+        return {
+            "IntercomDemo": lambda signals: None,
+            "IntercomAnalysis": lambda signals: self.notify_signals(
+                "IntercomAnalysis", [Signal({
+                    "document_tone": {
+                        "tone_categories": [{
+                            "category_id": "emotion_tone",
+                            "tones": [{"tone_name": "Anger", "score": 0.51},
+                                      {"tone_name": "Disgust", "score": 0.40}]
+                        }]
+                    }
+                })]
+            )
+        }
 
-    def setUp(self):
-        self.mock_modules()
-        super().setUp()
+    def test_slack_post(self):
 
-    # TEST:
-    # Intercom message pings watson
-    # Intercom message posts to slack
-    # MergeState and Filter blocks work
-    def test_function(self):
-        self.assertTrue(True)
+        self.notify_signals(
+            "IntercomDemo", [Signal({
+                "data": {
+                    "item": {
+                        "conversation_message": {
+                            "body": "<p>Hello!</p>"
+                        }
+                    }
+                }
+            })]
+        )
+        self.wait_for_processed_signals("PostIntercomToSlack", 1)
+
+        self.assertEqual(self.mocked_slack.call_count, 1)
+        self.assertEqual(self.mocked_slack.call_args_list[0][0],
+              ("#intercom_alerts", "Hello!"))
+
 
